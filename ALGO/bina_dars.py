b764fb1c17f0b98cf2c0e216ba023222962cda6d
andil <andildine2@gmail.com>
2023-02-18 21:53:35 +0100

    Choix des lettres

--- ALGO/bina_dars.py
+++ ALGO/bina_dars.py
@@ -1,18 +1,52 @@
 from dourous import *
 
 #Données
+def notNegativ(phrase):
+  num = int(input(phrase + ":"))
+  while(num < 0):
+    print("Pas négatif".upper())
+    num = int(input(phrase + ":"))
+  return num
+
+
+def choixDoubleLettres(phrase, a, b):
+  choix= input(phrase + " " +  a + " / " +  b + ": ")
+  while choix.upper() !=a and choix.upper() != b:
+    print("Choix invalide")
+    choix= input(phrase + " "  +  a + " / " +  b + ": ")
+  return choix.upper()
+
+
 
 
 
 
 def controlPersonnifieArguments():
-  awwal= int(input("Numéro du premier cours : "))
-  akhir= int(input("Numéro du dernier cours : "))
-  adad_al_kalimates= int(input("Nombre de mots : "))
-  harf_awwal= int(input("Nombre de lettres de la première série : "))
-  harf_akhir= int(input("Nombre de lettres de la dernière série : "))
 
-  return (awwal, akhir, adad_al_kalimates, harf_awwal, harf_akhir)
+  choix= choixDoubleLettres("Choisir les lettres" , "O", "N")
+  choix_lettres=False
+  
+  if choix == "O": 
+    choix2 = choixDoubleLettres("Lettres isolées ou attachées " , "I", "A")
+     
+    harf_awwal = 1 if choix2 == "I" else 3
+    harf_akhir = 1 if choix2 == "I" else 3
+    awwal, akhir = 7, 7
+    choix_lettres=True
+
+
+  else:
+    awwal= notNegativ("Numéro du premier cours")
+    akhir= notNegativ("Numéro du dernier cours")
+    harf_awwal= notNegativ("Nombre de lettres de la première série")
+    harf_akhir= notNegativ("Nombre de lettres de la dernière série")
+
+
+  #adad_al_kalimates= int(input("Nombre de mots : "))
+  adad_al_kalimates= notNegativ("Nombre de mots")
+
+  
+  return (awwal, akhir, adad_al_kalimates, harf_awwal, harf_akhir, choix_lettres)
 
 
 def controlDefinieArguments():
@@ -29,15 +63,15 @@ def controlDefinieArguments():
   while(dars < 0 or dars > 8):
     dars = int(input("Entrez le numéro : "))
   
-  niv1 = (1, 7, 10, 1, 1)
-  niv2 = (1, 7, 5, 3, 3)
-  niv3 = (8, 12, 10, 3, 3)
-  niv4 = (13, 18, 10, 3, 3)
+  niv1 = (1, 7, 10, 1, 1, False)
+  niv2 = (1, 7, 5, 3, 3, False)
+  niv3 = (8, 12, 10, 3, 3, False)
+  niv4 = (13, 18, 10, 3, 3, False)
 
-  exam_niv1 = (7, 7, 50, 1, 1)
-  exam_niv2 = (7, 7, 20, 3, 3)
-  exam_niv3 = (12, 12, 10, 3, 3)
-  exam_niv4 = (13, 18, 10, 3, 3)
+  exam_niv1 = (7, 7, 50, 1, 1, False)
+  exam_niv2 = (7, 7, 20, 3, 3, False)
+  exam_niv3 = (12, 12, 10, 3, 3, False)
+  exam_niv4 = (13, 18, 10, 3, 3, False)
 
 
   #Variables pour les tests
@@ -56,11 +90,42 @@ def ModeBinaDars():
     while(mode != "p" and mode!= 'd'):
         mode = input()
 
-    if(mode == "p"):
+    phrase = """
+    "Choississez le mode
+    Personnifié (Tapez p)
+    Défini (Tapez d)"
+    """ 
+    mode = choixDoubleLettres(phrase, "P", "D")
+
+    if(mode == "P"):
         return controlPersonnifieArguments()
     
-    if(mode == "d"):    
+    if(mode == "D"):    
         return  controlDefinieArguments()
+  
+
+
+def lettresDefinies(houroufs):
+  print("Choisissez des lettres")
+  houroufs = houroufs[0] + houroufs[1]
+  hourouf_moukhtaras=[]
+  
+  for i in range(len(houroufs)):
+    print(i, houroufs[i])
+
+  nb_lettres = int(input("Combien de lettres ? : "))
+  while nb_lettres < 0:
+    print("Nombre négatifs !")
+    nb_lettres = int(input("Combien de lettres ? : "))
+  for i in range(nb_lettres):
+    n = int(input("Sélectionnez une lettre : "))
+    if(n >= len(houroufs) or n < 0):
+      print("Chiffre non autorisé !")
+      continue
+    print("Vous avez choisi : ",houroufs[n]) 
+    hourouf_moukhtaras+= houroufs[n]
+
+  return hourouf_moukhtaras   
 
 """
 awwal : Premier Cours
@@ -70,14 +135,22 @@ harf_awwal : nombre de lettre de la première série
 harf_akhir : nombre de lettres de la dernière série
 
 """
-def COURS_BUILDER(awwal=1, akhir=12, adad_al_kalimates=5, harf_awwal=1, harf_akhir=3): 
-    if(awwal > akhir or awwal <= 0 or akhir > 18  ):
+def COURS_BUILDER(awwal=1, akhir=12, adad_al_kalimates=5, harf_awwal=1, harf_akhir=3, choix_lettres=False): 
+    
+    if(awwal > akhir or awwal <= 0 or akhir > 18):
       print("Erreur sur les numéros de Cours") ; return
-    if (adad_al_kalimates < 0  ):
+    if (adad_al_kalimates < 0):
       print("Erreur sur les numéros le nombre de mot") ; return
     if(harf_awwal > harf_akhir or harf_awwal <= 0):
       print("Erreur sur le nombre de lettres") ; return
   
-
     for numero_du_cours in CC[awwal-1:akhir]:
+      #capturer les lettres par "numero_du_cours"
+      if choix_lettres:  
+        hourouf_mourads = [numero_du_cours[1][0]] + [numero_du_cours[1][1]]
+        hourouf_mourads = lettresDefinies(hourouf_mourads)
+        n = len(hourouf_mourads)
+        hourouf_mourads = [hourouf_mourads[:n//2]] + [hourouf_mourads[n//2:]]
+        numero_du_cours[1] = hourouf_mourads
+
       print_cours(numero_du_cours, adad_al_kalimates, harf_awwal, harf_akhir)
--- ALGO/main_hourouf.py
+++ ALGO/main_hourouf.py
@@ -1,17 +1,21 @@
-from operator import mod
 from bina_dars import *
-
+    
 
 
 
 #initialisation des variables
-m = ModeBinaDars()
-#m = (1, 7, 10, 1, 1)
+m = ModeBinaDars() 
+
+
+#m = (1, 7, 10, 1, 1, False)
+#m = (6, 7, 10, 1, 1, True)
+
 premier_cours= m[0] 
 dernier_cours=m[1]
 nombre_mot=m[2]
 nombre_de_lettres_premiere_serie=m[3]
 nombre_de_lettres_derniere_serie=m[4]
+choix_lettres= m[5]
 
 
 
@@ -20,8 +24,12 @@ COURS_BUILDER(premier_cours,
               dernier_cours, 
               nombre_mot, 
               nombre_de_lettres_premiere_serie, 
-              nombre_de_lettres_derniere_serie 
+              nombre_de_lettres_derniere_serie, 
+              choix_lettres
+
 )
+
+
 
 f.close()
 
@@ -34,14 +42,24 @@ f.close()
 #DONE Séparation en dossiers
 #DONE Acceptation des arguments (os)
 #DONE Transformation du fichier
-#TODO Faciliter l'ajout de nouvelles leçons
+#TODO Creéer des niveaux avec lettres précises
 #TODO Recréer les 5 niveaux
 #TODO Première version d'un pdfs complet (PDF Merge)
 #TODO Exercice écriture contrôle
 
 
-
-#TODO Transformation du fichier
-#DONE ouvrir le fichier mas
-#DONE ecrire des choses dedans
-#DONE changer les print par des print
+#DONE Créer des niveaux avec lettres précises
+#DONE Si paramètre : faire un affichage
+#DONE Afficher les lettres en questiono
+#DONE Si paramètre : faire plusieurs lettres
+#DONE Fonction qui affiche les lettres
+#DONE Affiche avec les numéros
+#DONE créer une liste avec les lettres rentréres
+#DONE controller chiffres
+#DONE Si paramètre : faire les lettres de notre choix
+#DONE Si paramètre : Afficher les lettres voulues
+#DONE INTÉGRER LE MODE PERSONNIFIÉ LETTRES
+#DONE Changer récation apprès choix isol ou attaché (nombre lettres)
+#DONE voir les controles : analyser tous les input
+#DONE controller 
+#TODO Factoriser
